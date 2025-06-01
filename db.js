5dc83e88a9b8f91d901343d1bb997a43b7757e99
Akshay kumar <akshaycodex@gmail.com>
2025-06-01 17:26:31 +0530

    add MongoDB Atlass

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const ConnectDB= async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://akshaycodex:Software@143A@myappdb.qpclqow.mongodb.net/?retryWrites=true&w=majority&appName=MyAppDB")
         console.log("MongoDB Connected !!")
     } catch(error){
         console.error("Mongoose Connection Failed !!",error.message);
